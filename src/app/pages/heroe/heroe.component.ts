48ef3ad3253e1141470644cdda38947efa360159
Juan Luis Rastrollo Guerrero <juanlu@MacBook-Pro-de-Juan.local>
2020-03-25 16:51:55 +0100

    Final version

---
+++ src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor( private heroesService: HeroesService,
+               private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+
+    if (id !== 'nuevo') {
+      this.heroesService.getHeroe(id).subscribe( (resp: HeroeModel) => {
+        this.heroe = resp;
+        this.heroe.id = id;
+      });
+    }
+  }
+
+  guardar(f: NgForm) {
+
+    if (f.invalid) {
+     console.log('Formulario invalido');
+     return;
+    }
+
+    Swal.fire( {title: 'Espere', text: 'Guardando información', icon: 'info', allowOutsideClick: false });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if (this.heroe.id) {
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
+    } else {
+      peticion = this.heroesService.crearHeroe(this.heroe);
+    }
+
+    peticion.subscribe( resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      });
+    });
+
+  }
+
+}
